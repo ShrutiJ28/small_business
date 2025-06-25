381b976dc8720afcde7e8787dd14a9904e9c163e
ShrutiJ28 <shrutijiwatode1997@gmail.com>
2025-06-25 16:30:31 +0100

    RESTAPIs to perform operations

---
+++ small_business/src/main/java/com/shruti/first/demo/model/Product.java
@@ -0,0 +1,54 @@
+package com.shruti.first.demo.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private boolean productAvailable;
+    private int stockQuantity;
+    private String imageName;
+    private String imageType;
+
+    @Lob
+    private byte[] imageData;
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public void setImageType(String imageType) {
+        this.imageType = imageType;
+    }
+
+    public void setImageData(byte[] imageData) {
+        this.imageData = imageData;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+    public String getImageType() {
+        return imageType;
+    }
+    public byte[] getImageData() {
+        return imageData;
+    }
+}
